8a2cd7072d32d139a397ef4e94071c008e5d38a2
Tran Van Tuan Phong <tvtp11052002@gmail.com>
2025-06-01 16:44:31 +0700

    create endpoint for create user, retrieve user info and get list users

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,54 @@
 # FastAPI routes
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from fastapi import status, APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session, joinedload
+from typing import List
+from uuid import UUID
+from app import models, schemas
+from app.db import SessionLocal
+from app.models import User, Message, MessageRecipient
+from datetime import datetime
+
+router = APIRouter()
+
+# Dependency to get DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.post("/users/", response_model=schemas.UserResponse, status_code=status.HTTP_201_CREATED)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    
+    db_user = models.User(email=user.email, name=user.name)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+@router.get("/users/", response_model=list[schemas.UserResponse])
+def list_users(db: Session = Depends(get_db)):
+    return db.query(models.User).all()
+
+
+@router.get("/users/{user_id}", response_model=schemas.UserResponse)
+def get_user(user_id: UUID, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+@router.delete("/users/", status_code=status.HTTP_204_NO_CONTENT)
+def delete_all_users(db: Session = Depends(get_db)):
+    db.query(models.User).delete()
+    db.commit()
+    return {"detail": "All users deleted"}
+
