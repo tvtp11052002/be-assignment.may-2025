0c30b38dd04141570dc63311671dbb6c0acf458f
Tran Van Tuan Phong <tvtp11052002@gmail.com>
2025-06-01 16:39:16 +0700

    create table users, messages, message_recipients

--- app/models.py
+++ app/models.py
@@ -1,1 +1,47 @@
 # SQLAlchemy or Tortoise models
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import uuid
+from sqlalchemy import Column, String, DateTime, Text, Boolean, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from app.db import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    email = Column(String, unique=True, nullable=False)
+    name = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.now())
+
+    sent_messages = relationship("Message", back_populates="sender")
+    received_messages = relationship("MessageRecipient", back_populates="recipient")
+
+
+class Message(Base):
+    __tablename__ = "messages"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    sender_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
+    subject = Column(String)
+    content = Column(Text, nullable=False)
+    timestamp = Column(DateTime, default=datetime.now())
+
+    sender = relationship("User", back_populates="sent_messages")
+    recipients = relationship("MessageRecipient", back_populates="message")
+
+
+class MessageRecipient(Base):
+    __tablename__ = "message_recipients"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    message_id = Column(UUID(as_uuid=True), ForeignKey("messages.id"))
+    recipient_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
+    
+    read = Column(Boolean, default=False)
+    read_at = Column(DateTime, nullable=True)
+
+    message = relationship("Message", back_populates="recipients")
+    recipient = relationship("User", back_populates="received_messages")
