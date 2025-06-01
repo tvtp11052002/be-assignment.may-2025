f0493ca1489afc65ef1d4c9f7deb4ea752345a87
Tran Van Tuan Phong <tvtp11052002@gmail.com>
2025-06-01 16:37:57 +0700

    create engine, session to connect with Postgre

--- app/db.py
+++ app/db.py
@@ -1,1 +1,15 @@
 # DB connection setup
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
